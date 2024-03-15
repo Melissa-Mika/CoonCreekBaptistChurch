6a29776c4f1972b37aa2a395893d6118b764b9a6
Melissa-Mika <melissamika3@gmail.com>
2024-03-15 13:32:58 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coon Creek Baptist Church</title>
+
+<link rel= "stylesheet" href= "./view/styles.css">
+
+</head>
+<body>
+
+<?php include "includes/cdnlinks.php" ?>
+<?php include "includes/nav.php" ?>
+
+<h1>Coon Creek Baptist Church</h1>
+<h2>Gather at the Creek</h2>
+
+<img src = "view/images/CoonCreekLogo.jpeg" alt = "Coon Creek Baptist Church Logo">
+
+
+
+
+
+    
+</body>
+</html>
---
+++ view/ministries.php
@@ -0,0 +1,24 @@
+
+<?php include "../includes/cdnlinks.php" ?>
+<?php include "../includes/nav.php" ?>
+
+<link rel= "stylesheet" href= "../view/styles.css">
+
+<h1>Coon Creek Baptist Church</h1>
+<h3>Ministries</h3>
+
+<h1>TeamKid</h1>
+
+<p>TeamKID is Coon Creek Baptist Church’s kid’s ministry program. At TeamKID children , ages Pre-K through 5th grade, gather to Learn About God, Use the Bible, and Live for Jesus! The kids sing praise songs, share prayer requests, learn Bible lessons, participate in community service projects and play games with their peers. TeamKID strives to start children on a path in the local church of feeling included, encouraged, and excited about their part in the Kingdom of God. TeamKID meets Wednesdays from 6-8pm, beginning with supper. </p>
+
+
+
+<h1>Youth Group</h1>
+
+<p>Coon Creek Baptist Church youth group is a Bible-based, fun and safe environment for middle school and high school aged students. Our Youth Group gathers Wednesday evenings for supper, fellowship, games and Bible study from 6-8pm.  They actively participate in Christian-based community events and activities and attend church camps each summer. The group seeks to instill in the students a knowledge of who God is, who He calls them to be, as well as a love for and participation in the body of Christ.   </p>
+
+
+
+<h1>TLC Women's Group</h1>
+
+<p>TLC Women’s Group is available for all ladies the last Tuesday evening of the month.  TLC meets each month at different women’s homes within the church for a time of prayer and Bible study with others. It’s a time to bear one another’s burdens and joys in life and grow in unity as a body of believers.</p>
